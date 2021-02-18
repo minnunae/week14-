42e0627dec9bfc6ac9b6c0c6fa6e50d11c5c9cb6
minnunae <69183312+minnunae@users.noreply.github.com>
2021-02-18 12:09:54 +0700

    Add files via upload

---
+++ Test3Dog.java
@@ -0,0 +1,13 @@
+package dog.com.it;
+
+public class Test3Dog {
+	
+	public static void main(String[]args) {
+		//create object here
+		Dog Dog1 = new Dog("Bulldog ", "larg ", "light gray ", 5);
+		Dog1.methodDog1Display();
+		
+		Dog Dog2 = new Dog("Beagle", "larg ", "orangey",6);
+		Dog2.methodDog2Display();
+	}
+}
